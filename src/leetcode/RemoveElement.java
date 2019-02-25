61394f7d7e47d5873b3ea93d28d2ef7860342070
yjfanling <33656378+yjfanling@users.noreply.github.com>
2019-02-25 14:34:43 +0800

    27. Remove Element
    移除数组中与给定的数val相等的值

---
+++ src/leetcode/RemoveElement.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class RemoveElement {
+	public int removeElement(int[] nums, int val) {
+        int length=0;
+        for(int i=0;i<nums.length;i++)
+        {
+        	if(nums[i]!=val)
+        	{
+        		nums[length]=nums[i];
+        		length++;
+        	}
+        }
+        return length;
+    }
+}
