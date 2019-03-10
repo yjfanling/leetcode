f8573af919cad9fa207b0eacf8c0d29e72bbb370
yjfanling <33656378+yjfanling@users.noreply.github.com>
2019-03-10 22:22:17 +0800

    22.GenerateParentheses
    生成n组括号的全排列

--- src/leetcode/GenerateParentheses.java
+++ src/leetcode/GenerateParentheses.java
@@ -9,45 +9,26 @@ import java.util.Stack;
 import org.junit.Test;
 
 public class GenerateParentheses {
-	 public List<String> generateParenthesis(int n) {
-			List<String> list=new ArrayList<String>();
-			GenerateParenthesesdfs(list," ",n,n);
-			return list;
-	    }
+	public List<String> generateParenthesis(int n) {
+		List<String> list=new ArrayList<String>();
+		GenerateParenthesesdfs(list,"",n,n);
+		return list;
+    }
 
-		private void GenerateParenthesesdfs(List<String> list, String s, int left,int right) {
-			// TODO Auto-generated method stub
-			if(left>right)
-				return;
-			if(left==0&&right==0)
-				list.add(s);
-			else
-			{
-				if(left>0)
-					GenerateParenthesesdfs(list, s+'(', left-1, right);
-				if(right>0)
-					GenerateParenthesesdfs(list, s+')', left, right-1);
-			}
+	private void GenerateParenthesesdfs(List<String> list, String s, int left,int right) {
+		// TODO Auto-generated method stub
+		if(left>right)
+			return;
+		if(left==0&&right==0)
+			list.add(s);
+		else
+		{
+			if(left>0)
+				GenerateParenthesesdfs(list, s+'(', left-1, right);
+			if(right>0)
+				GenerateParenthesesdfs(list, s+')', left, right-1);
 		}
-		public List<String> generateParenthesis1(int n) {
-	        Set<String> res = new HashSet<String>();
-	        if (n == 0) {
-	            res.add("");
-	        } else {
-	            List<String> pre = generateParenthesis(n - 1);
-	            for (String str : pre) {
-	                for (int i = 0; i < str.length(); ++i) {
-	                    if (str.charAt(i) == '(') {
-	                        str = str.substring(0, i + 1) + "()" + str.substring(i + 1, str.length());
-	                        res.add(str);
-	                        str = str.substring(0, i + 1) +  str.substring(i + 3, str.length());
-	                    }
-	                }
-	                res.add("()" + str);
-	            }
-	        }
-	        return new ArrayList(res);
-	    }
+	}
 		
 	@Test
 	public void test(){
