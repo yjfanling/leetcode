e60be5537e6cd4a94e59e05a4383b88b5f337b08
yjfanling <33656378+yjfanling@users.noreply.github.com>
2019-03-28 14:51:46 +0800

    91.Decode Ways

--- src/leetcode/DecodeWays.java
+++ src/leetcode/DecodeWays.java
@@ -1,5 +1,21 @@
 package leetcode;
 
 public class DecodeWays {
+	public int numDecodings(String s) {
+        if(s.length()==0||(s.length()==1&&s.charAt(0)=='0'))
+            return 0;
+        if(s.length()==1)
+            return 1;
+        int[] dp=new int[s.length()+1];
+        dp[0]=1;
+        for(int i=0;i<s.length();i++)
+        {
+            dp[i+1]=s.charAt(i)=='0'?0:dp[i];
+            if(i>0&&(s.charAt(i-1)=='1'||(s.charAt(i-1)=='2'&&s.charAt(i)<='6'))){
+                dp[i+1]+=dp[i-1];
+            }
+        }
+        return dp[s.length()];
+    }
 
 }
